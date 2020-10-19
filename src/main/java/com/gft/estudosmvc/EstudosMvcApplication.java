ae2ef4ece5999d950932579695d5232386d7612f
lspt <lucas.pedroso@gft.com>
2020-10-19 16:30:56 -0300

    Initial setup and CadastroTitulo Controller and template

---
+++ src/main/java/com/gft/estudosmvc/EstudosMvcApplication.java
@@ -0,0 +1,13 @@
+package com.gft.estudosmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosMvcApplication.class, args);
+	}
+
+}
