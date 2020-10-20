153c4c25e967834212f532d7f8926b28d63ef44f
lspt <lucas.pedroso@gft.com>
2020-10-20 13:12:53 -0300

    connection to mysql

--- src/main/java/com/gft/estudosmvc/controller/TituloController.java
+++ src/main/java/com/gft/estudosmvc/controller/TituloController.java
@@ -1,10 +1,15 @@
 package com.gft.estudosmvc.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
+import com.gft.estudosmvc.model.StatusTitulo;
 import com.gft.estudosmvc.model.Titulo;
 import com.gft.estudosmvc.repository.Titulos;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,8 +22,9 @@ public class TituloController {
 	private Titulos titulos;
 
 	@RequestMapping("/novo")
-	public String pageNovoTitulo() {
-		return "CadastroTitulo";
+	public ModelAndView pageNovoTitulo() {
+		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		return mv;
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
@@ -28,5 +34,10 @@ public class TituloController {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
 		mv.addObject("mensagem", "Titulo salvo com sucesso!");
 		return mv;
+	}
+
+	@ModelAttribute("allStatusTitulo")
+	public List<StatusTitulo> allStatusTitulo() {
+		return Arrays.asList(StatusTitulo.values());
 	}
 }
