e241a00e1156aa437038e08711a8cb5db9973868
lspt <lucas.pedroso@gft.com>
2020-10-21 11:30:48 -0300

    2.15. Implementando a edição

--- src/main/java/com/gft/estudosmvc/controller/TituloController.java
+++ src/main/java/com/gft/estudosmvc/controller/TituloController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -22,12 +23,14 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 @RequestMapping("/titulos")
 public class TituloController {
 
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
+
 	@Autowired
 	private Titulos titulos;
 
 	@RequestMapping("/novo")
 	public ModelAndView pageNovoTitulo() {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject(new Titulo());
 		return mv;
 	}
@@ -35,7 +38,7 @@ public class TituloController {
 	@PostMapping
 	public String save(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		if (errors.hasErrors()) {
-			return "CadastroTitulo";
+			return CADASTRO_VIEW;
 		}
 
 		titulos.save(titulo);
@@ -50,6 +53,15 @@ public class TituloController {
 		List<Titulo> allTitulos = titulos.findAll();
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		mv.addObject("titulos", allTitulos);
+		return mv;
+	}
+
+	@GetMapping("{id}")
+	public ModelAndView edit(@PathVariable("id") Titulo titulo) {
+
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
+
 		return mv;
 	}
 
