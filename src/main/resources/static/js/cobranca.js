535552388bfef87fced8ded300d0c50eb9131d49
lspt <lucas.pedroso@gft.com>
2020-10-21 20:33:59 -0300

    3.3. Máscara de entrada de valores

--- src/main/resources/static/js/cobranca.js
+++ src/main/resources/static/js/cobranca.js
@@ -26,4 +26,9 @@ $("#confirmacaoExclusaoModal").on("show.bs.modal", function (event) {
 
 $(function () {
   $('[rel="tooltip"]').tooltip();
+  $(".js-currency").maskMoney({
+    decimal: ",",
+    thousands: ".",
+    allowZero: true,
+  });
 });
